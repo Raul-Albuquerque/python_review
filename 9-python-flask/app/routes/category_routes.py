fa275a2f991de2fda9e709551f9ee603e95c90b1
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2026-03-26 00:16:52 -0300

    feat: add pydantic type validation

---
+++ 9-python-flask/app/routes/category_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify
+
+category_bp = Blueprint("category_bp", __name__, url_prefix="/categories")
+
+
+@category_bp.route("/", methods=["GET"])
+def get_categories():
+    return jsonify({"message": "Retorna a lista de todas as categorias."})
+
+
+@category_bp.route("/", methods=["POST"])
+def create_category():
+    return jsonify({"message": "Retorna a lista de todas as categorias."})
